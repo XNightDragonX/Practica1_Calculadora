c085c73f389b9b333a23634a37622291d3b2a351
PC <xnightdragonx@outlook.com>
2021-08-30 11:37:47 -0500

    add new method and new variable

---
+++ app/src/main/java/ipn/cecyt9/practica1_calculadora/MainActivity.java
@@ -0,0 +1,122 @@
+package ipn.cecyt9.practica1_calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    Double numero1, numero2, resultado;
+    String operador;
+
+    public void onClickCButton(View miView){
+        TextView tv = findViewById(R.id.Res);
+        tv.setText("");
+    }
+
+    public void onClickButtonZero(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "0");
+    }
+
+    public void onClickButtonOne(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "1");
+    }
+
+    public void onClickButtonTwo(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "2");
+    }
+
+    public void onClickButtonThree(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "3");
+    }
+
+    public void onClickButtonFour(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "4");
+    }
+
+    public void onClickButtonFive(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "5");
+    }
+
+    public void onClickButtonSix(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "6");
+    }
+
+    public void onClickButtonSeven(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "7");
+    }
+
+    public void onClickButtonEight(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "8");
+    }
+
+    public void onClickButtonNine(View miView){
+        TextView tv = findViewById(R.id.Res);
+        String value = tv.getText().toString();
+        tv.setText(value + "9");
+    }
+
+    public void onClickNum1captureoperation(View miView) {
+        TextView tv = findViewById(R.id.Res);
+        numero1 = Double.parseDouble(tv.getText().toString());
+        tv.setText("");
+    }
+
+    public void onClickPlus(View miView){
+        operador="+";
+        onClickNum1captureoperation(miView);
+    }
+
+    public void onClickMinus(View miView){
+        operador="-";
+        onClickNum1captureoperation(miView);
+    }
+
+    public void onClickequals(View miView){
+        TextView tv = (TextView) findViewById(R.id.Res);
+        numero2 = Double.parseDouble(tv.getText().toString());
+
+        try {
+
+            if (operador.equals("+")) {
+                resultado = numero1 + numero2;
+                else if (operador.equals("-")) {
+                    resultado = numero1 - numero2;
+                }
+            }
+            tv.setText(resultado.toString());
+        }
+        catch(NumberFormatException nfe) {
+            Toast.makeText( this, "Numero Incorrecto", Toast.LENGTH_SHORT).show();
+        }
+        resultado = numero1;
+    }
+
+}
